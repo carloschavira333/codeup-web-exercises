02845e08a5ca36d33fdcc846e4568ba9cf3e6772
carlos.chavira333 <carlos.chavira333@gmail.com>
2022-03-24 09:29:27 -0500

    Push update: Completed warm up ex.

--- js/loops.js
+++ js/loops.js
@@ -4,19 +4,15 @@
 // 7 x 1 = 7
 // through
 // 7 x 10 = 70
+
 // function showMultiplicationTable(n) {
-//     console.log(n + " x 1 = " + n * 1);
-//     console.log(n + " x 2 = " + n * 2);
-//     console.log(n + " x 3 = " + n * 3);
-//     console.log(n + " x 4 = " + n * 4);
-//     console.log(n + " x 5 = " + n * 5);
-//     console.log(n + " x 6 = " + n * 6);
-//     console.log(n + " x 7 = " + n * 7);
-//     console.log(n + " x 8 = " + n * 8);
-//     console.log(n + " x 9 = " + n * 9);
-//     console.log(n + " x 10 = " + n * 10);
+//
+//     for (var i = 0; i < 11; i++) {
+//         var result = n * i;
+//         console.log(n + " X " + i + " = " + result);
+//     }
 // }
-// showMultiplicationTable(1);
+// showMultiplicationTable(2);
 
 // 3. todo: Use a for loop and the code from the previous lessons to generate 10 random numbers between 20 and 200 and output to the console whether each number is odd or even. For example:
 // 123 is odd
@@ -28,14 +24,12 @@
 
 
 // for (var x=0; x<=9; x++) {
-//         var randomNum = Math.floor(Math.random());
-//     // console.log(x * randomNum+20)
-//     var xPlusRandom = (x * randomNum+20)
+//     var randomNum = Math.floor(Math.random() * (200 - 20)) + 20;
 //
-//     if ( xPlusRandom % 2 === 0) {
-//         console.log(xPlusRandom + " is Even")
+//     if (randomNum % 2 === 0) {
+//         console.log(randomNum + " is Even")
 //     } else {
-//         console.log(xPlusRandom + " Number is odd")
+//         console.log(randomNum + " is Odd")
 //     }
 // };
 
@@ -74,6 +68,9 @@
 // }
 //
 // };
+// The below for loop also produces the expected results
+// for (var i = 1; i<=9; i++){
+//     console.log((i + "").repeat(i))}
 
 // todo Create a for loop that uses console.log to create the output shown below.
 // 100
@@ -97,15 +94,72 @@
 // 10
 // 5
 
-for (var i = 100; i > 0; i -= 5) {
-        console.log(i)
+// for (var i = 100; i > 0; i -= 5) {
+//         console.log(i)
+// }
+
+// TODO Do While Loop
+// An ice cream seller can't go home until she sells all of her cones. First write enough code that generates a random number between 50 and 100 representing the amount of cones to sell before you start your loop. Inside of the loop your code should generate another random number between 1 and 5, simulating the amount of cones being bought by her clients. Use a do-while loop to log to the console the amount of cones sold to each person. The below code shows how to get the random numbers for this exercise.
+
+// 5 cones sold...  // if there are enough cones
+// Cannot sell you 6 cones I only have 3...  // If there are not enough cones
+// Yay! I sold them all! // If there are no more cones
+
+// This is how you get a random number between 50 and 100
+function coneStore() {
+    var coneInventory = Math.floor(Math.random() * 50) + 50;
+    console.log("total" + coneInventory)
+
+    do {
+        var amtBought = Math.floor(Math.random() * 5) + 1;
+        // var amtBought = prompt("enter a number");
+
+        coneInventory = coneInventory - amtBought;
+        console.log("coneInventory" + coneInventory)
+        console.log("amtBought" + amtBought)
+
+    } while (coneInventory >= 6)
+
+    do {
+        var nextDayCones = prompt("Enter a number:")
+        if (coneInventory > nextDayCones) {
+            console.log(nextDayCones + " cones sold")
+        } else if (coneInventory < nextDayCones) {
+            console.log("Cannot sell you " + nextDayCones + " cones I only have " + coneInventory + " left.")
+        } else {
+            console.log("Yay! I sold them all!")
+        }
+    } while ()
+
+
 }
 
+coneStore()
 
 
-
-for (var i = 1; i<=9; i++){
-    console.log((i).repeat(i))}
-
-for (var i = 1; i<=9; i++){
-    console.log((i + ",").repeat(i))}
+// Good as of 940pm
+// This is how you get a random number between 50 and 100
+// function coneStore() {
+//     var coneInventory = Math.floor(Math.random() * 50) + 50;
+//     console.log("total" + coneInventory)
+//
+//     do {
+//         var amtBought = Math.floor(Math.random() * 5) + 1;
+//         // var amtBought = prompt("enter a number");
+//
+//         coneInventory = coneInventory - amtBought;
+//         console.log("coneInventory" + coneInventory)
+//         console.log("amtBought" + amtBought)
+//
+//     } while (coneInventory >= 6)
+//     var nextDayCones = prompt("Enter a number:")
+//     if (coneInventory > nextDayCones) {
+//         console.log(nextDayCones + " cones sold")
+//     } else if (coneInventory < nextDayCones) {
+//         console.log("Cannot sell you " + nextDayCones + " cones I only have " + coneInventory + " left.")
+//     } else {
+//         console.log("Yay! I sold them all!")
+//     }
+// }
+//
+// coneStore()
--- js/warmups.js
+++ js/warmups.js
@@ -3,19 +3,19 @@
 // Write a javascript function that accepts the 3 lengths of a triangle and outputs the triangle’s area.
 
 // Two Inputs: Base * Height divided by 2
-var baseValue = 5;
+/*var baseValue = 5;
 var heightValue = 5;
 
 function triangle(base, height) {
     return (base * height) / 2;
 }
 
-console.log(triangle(baseValue, heightValue));
+console.log(triangle(baseValue, heightValue));*/
 
 // Three inputs: Side a, b, c
 // Write a javascript function that accepts the 3 lengths of a triangle and outputs the triangle’s area.
 
-var sideA = 5;
+/*var sideA = 5;
 var sideB = 5;
 var sideC = 5;
 
@@ -23,5 +23,21 @@ function triangle2(a, b, c) {
     return a * b * c;
 }
 
-console.log(triangle2(sideA, sideB, sideC));
+console.log(triangle2(sideA, sideB, sideC));*/
 
+// Imagine the supplied array is like a pack of cards. Write a function to shuffle an array in to a completely new and entirely random order, much like shuffling a pack of cards.
+//     example input: [2,4,6,8]
+// expected output: [4,2,8,6];
+
+var numbersArray = [2, 4, 6, 8];
+
+function shuffArr(array) {
+    for (var i = numbersArray.length -1; i > 0; i--) {
+        var j = Math.floor(Math.random() * i);
+        var temp = numbersArray[i];
+        numbersArray[i] = numbersArray[j];
+        numbersArray[j] = temp;
+    }
+    return numbersArray
+}
+console.log(shuffArr())
