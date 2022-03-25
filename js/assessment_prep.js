2c175f9e63a0a8e2d5de4f235f15e14112ebbf71
carlos.chavira333 <carlos.chavira333@gmail.com>
2022-03-25 17:13:35 -0500

    Push update: Working on isOdd

--- js/assessment_prep.js
+++ js/assessment_prep.js
@@ -5,36 +5,36 @@
 // ## Problems
 // 1. Create a function named `calcAge` that takes the age and return the age in days. If input is not numeric, calcAge should return false.
 // function calcAge(age) {
-//     console.log("arg is a typeOf " + typeof(age) + " arg entered was: " + age)
 //     if (typeof(age) === 'number') {
 //         var ageInDays = (age * 365);
+//         return  ageInDays + " is your age in days"
 //     } else {
 //         return false
 //     }
-//     return ageInDays
 // }
-//
 // console.log(calcAge(1));
-
-function calcAge(age) {
-    console.log("arg is a typeOf " + typeof(age) + " arg entered was: " + age)
-    if (age !== Number) {
-        var ageInDays = (age * 365);
-    } else {
-        return false
-    }
-    return ageInDays
-}
-
-console.log(calcAge(1));
 
 //
 //
 // 2. Create a function named `increment` that takes a number as an argument, increments the number by +1 and returns the result. If input is not numeric, increment should return false.
-//
+// function increment(number) {
+//     if (typeof(number) === 'number') {
+//         var newNumber = number + 1;
+//         return newNumber
+//     } else {
+//         return false
+//     }
+// }
+// console.log(increment(0);
+
 //
 //
 // 3. Define a function named `isOdd` that takes in a number, return true is that number is odd. Return false if the number is either not a number or even.
+function isOdd(number) {
+    console.log(number + ' ' +typeof(number))
+
+}
+console.log(isOdd(false))
 //
 //
 // 4. Write a function named `oddArr`. Given the array of var arry = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] return the odd numbers.
