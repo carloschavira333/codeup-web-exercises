641d9c1fef45978021652ee3da493033cc69cfb1
carlos.chavira333 <carlos.chavira333@gmail.com>
2022-03-25 11:50:08 -0500

    Push update: Completed calcAge exercise.

---
+++ js/assessment_prep.js
@@ -0,0 +1,50 @@
+// # JavaScript Second reassessment Review
+// ## Instructions
+// 0. *Carefully* read *all* the instructions and problems before beginning your work.
+//
+// ## Problems
+// 1. Create a function named `calcAge` that takes the age and return the age in days. If input is not numeric, calcAge should return false.
+// function calcAge(age) {
+//     console.log("arg is a typeOf " + typeof(age) + " arg entered was: " + age)
+//     if (typeof(age) === 'number') {
+//         var ageInDays = (age * 365);
+//     } else {
+//         return false
+//     }
+//     return ageInDays
+// }
+//
+// console.log(calcAge(1));
+
+function calcAge(age) {
+    console.log("arg is a typeOf " + typeof(age) + " arg entered was: " + age)
+    if (age !== Number) {
+        var ageInDays = (age * 365);
+    } else {
+        return false
+    }
+    return ageInDays
+}
+
+console.log(calcAge(1));
+
+//
+//
+// 2. Create a function named `increment` that takes a number as an argument, increments the number by +1 and returns the result. If input is not numeric, increment should return false.
+//
+//
+//
+// 3. Define a function named `isOdd` that takes in a number, return true is that number is odd. Return false if the number is either not a number or even.
+//
+//
+// 4. Write a function named `oddArr`. Given the array of var arry = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] return the odd numbers.
+//
+//
+// 5. Define a function named `creditCard` that takes a credit card number and only displays the last four characters. The rest of the card number must be replaced by ************.
+//
+//
+// 6. create a function named `AlphabetSoup` that takes a string and returns a string with its letters in alphabetical order. Return false if string is empty or null
+//
+//
+// 7. create a function named `reverse` that takes in and reverses an array.
+// # regulus-second-js-reassessment-review
--- js/loops.js
+++ js/loops.js
@@ -106,33 +106,33 @@
 // Yay! I sold them all! // If there are no more cones
 
 // This is how you get a random number between 50 and 100
-function coneStore() {
+// function coneStore() {
     var coneInventory = Math.floor(Math.random() * 50) + 50;
     console.log("total" + coneInventory)
 
     do {
         var amtBought = Math.floor(Math.random() * 5) + 1;
-        // var amtBought = prompt("enter a number");
-
         coneInventory = coneInventory - amtBought;
-        console.log("coneInventory" + coneInventory)
-        console.log("amtBought" + amtBought)
+        // console.log("coneInventory" + coneInventory)
+        // console.log("amtBought" + amtBought)
 
     } while (coneInventory >= 6)
 
-    do {
+    // do {
         var nextDayCones = prompt("Enter a number:")
         if (coneInventory > nextDayCones) {
-            console.log(nextDayCones + " cones sold")
+            //if coneInventory is greater than nextDayCones then I have enough to sell
+            console.log(nextDayCones + " ")
+            // line 127 string to read more like 130
         } else if (coneInventory < nextDayCones) {
             console.log("Cannot sell you " + nextDayCones + " cones I only have " + coneInventory + " left.")
         } else {
             console.log("Yay! I sold them all!")
         }
-    } while ()
+    // } while ()
 
 
-}
+// }
 
 coneStore()
 
