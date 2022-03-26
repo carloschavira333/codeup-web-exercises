555c6d706b82a087d2644fefbd8c2fc4dc20a9c4
carlos.chavira333 <carlos.chavira333@gmail.com>
2022-03-25 23:54:57 -0500

    Push update: Completed 1 to 5.

--- js/assessment_prep.js
+++ js/assessment_prep.js
@@ -30,17 +30,48 @@
 //
 //
 // 3. Define a function named `isOdd` that takes in a number, return true is that number is odd. Return false if the number is either not a number or even.
-function isOdd(number) {
-    console.log(number + ' ' +typeof(number))
+// function isOdd(number) {
+//     console.log("arg enter is: " + number + ' and is a typeOf: ' +typeof(number))
 
-}
-console.log(isOdd(false))
+//     if (typeof(number) === 'number' && number % 2 !== 0) {
+//         return true
+//     } else {
+//         return false
+//     }
+// }
+// console.log(isOdd(2));
+
 //
 //
 // 4. Write a function named `oddArr`. Given the array of var arry = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] return the odd numbers.
+
+// function oddArr() {
+// var arry = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// var oddNumbers = arry.filter(number => number % 2 !== 0);
+// // console.log(oddNumbers);
+// return oddNumbers.toString();
+
+//     // for (i = 0; i < arry.length; i++) {
+//     //     if(arry[i] % 2 !== 0) {
+//     //         console.log(arry[i])
+//     //     }
+//     // }
+// }
+
+// console.log(oddArr());
+
 //
 //
 // 5. Define a function named `creditCard` that takes a credit card number and only displays the last four characters. The rest of the card number must be replaced by ************.
+// function creditCard(ccNumber) {
+//     var cardNumberInput = ccNumber;
+//     // Convert to array and replace
+//     var cardNumber = Array.from(cardNumberInput.toString()).map(Number);
+//     var lastFour = cardNumber.fill('*', 0, 12).join("");
+//     return lastFour;
+// }
+// console.log(creditCard(1234123412345578));
+
 //
 //
 // 6. create a function named `AlphabetSoup` that takes a string and returns a string with its letters in alphabetical order. Return false if string is empty or null
